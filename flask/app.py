4422e4adca4452b6693005e2711c5e0da1249495
Lydia <lydiahjchung@gmail.com>
2020-12-04 16:37:16 +0900

    plotly check

--- flask/app.py
+++ flask/app.py
@@ -4,98 +4,73 @@ import plotly
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
+from pfo.preprocess import find_all, get_value, save_pfos
 from pfo.allocation import Asset, Portfolio
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-final_pfo = dict()
-pfo_num = -1
+def create_plot(feature):
+    if feature == 'Bar':
+        N = 40
+        x = np.linspace(0, 1, N)
+        y = np.random.randn(N)
+        df = pd.DataFrame({'x': x, 'y': y})
+
+        data = [
+            go.Bar(
+                x=df['x'],
+                y=df['y']
+            )
+        ]
+    
+    else:
+        N = 1000
+        random_x = np.random.randn(N)
+        random_y = np.random.randn(N)
+
+        data = [
+            go.Scatter(
+                x = random_x,
+                y = random_y,
+                mode = 'markers'
+            )
+        ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/bar', methods=['GET', 'POST'])
+def change_features():
+    print('working', file=sys.stderr)
+    feature = request.args['selected']
+    graphJSON = create_plot(feature)
+    return graphJSON
+
 @app.route('/post', methods=['POST'])
 def post():
-    # if request.method == "POST":
-    data = json.loads(request.data.decode('UTF-8'))
-    
-    start, end = data["start"], data["end"]
-    initial, rebalancing = int(data["initial"]), int(data["rebalancing"])
+    # Allocating PFOS
+    total = request.get_data(as_text=True).split('&')
 
-    portfolios = []
-    pfo_num = len(data["profiles"])
+    start = get_value(total[find_all(total, "start")[0]])
+    end = get_value(total[find_all(total, "end")[0]])
+    initial = get_value(total[find_all(total, "initial")[0]])
+    rebalancing = get_value(total[find_all(total, "rebalancing")[0]])
+    print(start, end, initial, rebalancing, file=sys.stderr)
 
-    for i in range(len(data["profiles"])):
-        pfo = data["profiles"][i]
-        pfo_name = pfo["profile_name"]
+    pfo_idx = find_all(total, "pfo_name")
+    pfos = save_pfos(total, pfo_idx)
+    print(pfos, file=sys.stderr)
 
-        assets, tickers, asset_names, weights, leverages = [], [], [], [], []
+    feature='Bar'
+    bar = create_plot(feature)
 
-        for j in range(len(pfo["assets"])):
-            tickers.append(pfo["assets"][j]["ticker"])
-            asset_names.append(pfo["assets"][j]["asset_name"])
-            weights.append(pfo["assets"][j]["weight"])
-            leverages.append(pfo["assets"][j]["leverage"])
-            
-        tickers = [t for t in tickers if t]
-        asset_names = [n for n in asset_names if n]
-        weights = [float(w) for w in weights if w]
-        leverages = [int(l) for l in leverages if l]
-
-        lens = [len(tickers), len(weights), len(leverages)]
-        assert len(set(lens)) == 1
-        
-        for k in range(len(tickers)):
-            asset = Asset(tickers[k], asset_names[k], start, end)
-            assets.append(asset)
-        
-        portfolios.append(Portfolio(pfo_name, assets, weights, leverages, initial, rebalancing))
-        final_pfo[i] = Portfolio(pfo_name, assets, weights, leverages, initial, rebalancing)
-        
-    print(1, final_pfo, file=sys.stderr)
-
-        # return render_template('result.html', tables=[backtest_df.to_html(classes='data')], titles=backtest_df.columns.values)
-
-
-    # start = request.form.get("start_date")
-    # end = request.form.get("end_date")
-    # print(start, end, file=sys.stderr)
-    # exit()
-    # tickers = [t for t in request.form.getlist("asset[]") if t]
-    # names = [n for n in request.form.getlist("name[]") if n]
-    # weights = [int(w) for w in request.form.getlist("weight[]") if w]
-    # leverages = [int(l) for l in request.form.getlist("leverage[]") if l]
-    
-    # lens = [len(tickers), len(weights), len(leverages)]
-    # assert len(set(lens)) == 1
-
-    # assets = []
-    # for i in range(len(tickers)):
-    #     asset = Asset(tickers[i], names[i], start, end)
-    #     assets.append(asset)
-    # print(len(assets), assets[0].get_name(), assets[1].get_name(), file=sys.stderr)
-
-    # return render_template("result.html", assets=assets, weights=weights, leverages=leverages)
-
-    print(portfolios[0], file=sys.stderr)
-    # backtest_df = portfolios[0].backtest()
-    # print(backtest_df, file=sys.stderr)
-
-    return render_template('result.html')
-
-@app.route('/result')
-def result():
-
-    # 여기 걸어봤자 이미 저쪽은 리다이렉트되어있는거라 아무런 도움이 안됨 시벌
-    while True:
-        if len(final_pfo) == pfo_num:
-            print(2, final_pfo, file=sys.stderr)
-            return render_template('result.html')
-
-    # return render_template('result.html')
-    # return render_template("result.html", tables=[backtest_df.to_html(classes='data')], titles=backtest_df.columns.values)
+    return render_template('result.html', result=pfos, plot=bar)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
---
+++ flask/pfo/preprocess.py
@@ -0,0 +1,38 @@
+def find_all(data, string):
+    result = []
+    for i in range(len(data)):
+        if data[i].find(string) != -1:
+            result.append(i)
+    
+    return result
+
+def get_value(string):
+    if string.find('=') != -1:
+        return string[string.find('=')+1:]
+
+def get_pfo_info(data):
+    pfo = dict()
+    
+    ticker = [get_value(data[i]) for i in find_all(data, "asset_ticker")]
+    name = [get_value(data[i]) for i in find_all(data, "asset_name")]
+    weight = [float(get_value(data[i])) for i in find_all(data, "asset_weight")]
+    leverage = [int(get_value(data[i])) for i in find_all(data, "asset_leverage")]
+    
+    pfo['tickers'] = ticker
+    pfo['names'] = name
+    pfo['weights'] = weight
+    pfo['leverages'] = leverage
+    
+    return pfo
+
+def save_pfos(data, idx):
+    result = dict()
+    for i in range(len(idx)):
+        if i != len(idx)-1:
+            temp = [data[j] for j in range(idx[i], idx[i+1])]
+            result[get_value(temp[0])] = get_pfo_info(temp)
+        else:
+            temp = data[idx[i]:]
+            result[get_value(temp[0])] = get_pfo_info(temp)
+    
+    return result
