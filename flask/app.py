a2dcacf15eb442eda68f76661775ff256b73c9f2
Lydia <lydiahjchung@gmail.com>
2020-12-05 23:44:27 +0900

    visualize

--- flask/app.py
+++ flask/app.py
@@ -10,34 +10,61 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-def create_plot(feature):
-    if feature == 'Bar':
-        N = 40
-        x = np.linspace(0, 1, N)
-        y = np.random.randn(N)
-        df = pd.DataFrame({'x': x, 'y': y})
+portfolio = []
 
-        data = [
-            go.Bar(
-                x=df['x'],
-                y=df['y']
+def create_plot(feature, portfolio):
+    if feature == 'Drawdown':
+        data = []
+        for i in range(len(portfolio)):
+            date = portfolio[i].get_date()
+            result = portfolio[i].get_backtest_result()
+            data.append(
+                go.Scatter(
+                    x = date, 
+                    y = result.loc[:, 'Total Drawdown'],
+                    mode = 'lines',
+                    name = portfolio[i].get_name()
+                )
             )
-        ]
+        
+        layout = go.Layout(
+            title = 'Portfolio Drawdown Comparison',
+        )
     
-    else:
-        N = 1000
-        random_x = np.random.randn(N)
-        random_y = np.random.randn(N)
-
-        data = [
-            go.Scatter(
-                x = random_x,
-                y = random_y,
-                mode = 'markers'
+    elif feature == 'Cumreturn':
+        data = []
+        for i in range(len(portfolio)):
+            date = portfolio[i].get_date()
+            result = portfolio[i].get_backtest_result()
+            data.append(
+                go.Scatter(
+                    x = date, 
+                    y = result.loc[:, 'Total Cum. Return'],
+                    mode = 'lines',
+                    name = portfolio[i].get_name()
+                )
             )
-        ]
+        
+        layout = {}
 
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    else: # Balance
+        X = [[0,1,2], [0,1,2]]
+        Y = [[3,6,9], [8,6,4]]
+        
+        data = []
+        for i in range(len(X)):
+            data.append(
+                go.Scatter(
+                    x = X[i], 
+                    y = Y[i],
+                    mode='lines'
+                )
+            )
+
+        layout = {}
+
+    entire = [data, layout]
+    graphJSON = json.dumps(entire, cls=plotly.utils.PlotlyJSONEncoder)
 
     return graphJSON
 
@@ -45,11 +72,12 @@ def create_plot(feature):
 def index():
     return render_template('index.html')
 
-@app.route('/bar', methods=['GET', 'POST'])
+@app.route('/plot', methods=['GET', 'POST'])
 def change_features():
     print('working', file=sys.stderr)
     feature = request.args['selected']
-    graphJSON = create_plot(feature)
+    print("check", portfolio, file=sys.stderr)
+    graphJSON = create_plot(feature, portfolio)
     return graphJSON
 
 @app.route('/post', methods=['POST'])
@@ -59,16 +87,26 @@ def post():
 
     start = get_value(total[find_all(total, "start")[0]])
     end = get_value(total[find_all(total, "end")[0]])
-    initial = get_value(total[find_all(total, "initial")[0]])
-    rebalancing = get_value(total[find_all(total, "rebalancing")[0]])
-    print(start, end, initial, rebalancing, file=sys.stderr)
+    initial = int(get_value(total[find_all(total, "initial")[0]]))
+    rebalancing = int(get_value(total[find_all(total, "rebalancing")[0]]))
 
     pfo_idx = find_all(total, "pfo_name")
     pfos = save_pfos(total, pfo_idx)
-    print(pfos, file=sys.stderr)
 
-    feature='Bar'
-    bar = create_plot(feature)
+    for name in pfos.keys():
+        assets = []
+        data = pfos[name]
+
+        for i in range(len(data['tickers'])):
+            assets.append(Asset(data['tickers'][i], data['names'][i], start, end))
+        
+        temp = Portfolio(name, assets, data['weights'], data['leverages'], initial, rebalancing)
+        temp.backtest()
+        temp.backtest_result()
+        portfolio.append(temp)
+
+    feature='Drawdown'
+    bar = create_plot(feature, portfolio)
 
     return render_template('result.html', result=pfos, plot=bar)
 
